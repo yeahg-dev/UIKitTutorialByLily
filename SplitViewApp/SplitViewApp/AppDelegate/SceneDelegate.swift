49a5a483e358006025531cbf6278410e5d487cf2
yeahg-dev <yeahg.dev@gmail.com>
2022-02-13 15:56:51 +0900

    feat: tableView 구현 및 레이아웃 설정

--- SplitViewApp/SplitViewApp/AppDelegate/SceneDelegate.swift
+++ SplitViewApp/SplitViewApp/AppDelegate/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let splitViewController = SplitViewController()
+        let splitViewController = SplitViewController(style: .doubleColumn)
 
         window?.rootViewController = splitViewController
         window?.makeKeyAndVisible()
--- SplitViewApp/SplitViewApp/Controller/TableViewController.swift
+++ SplitViewApp/SplitViewApp/Controller/TableViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class TableViewController: UIViewController {
+    let tableView: UITableView = {
+        var tableView = UITableView(frame: .zero)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    override func loadView() {
+        view = .init()
+        view.backgroundColor = UIColor(red: 230/255, green: 231/255, blue: 156/255, alpha: 1)
+        view.addSubview(tableView)
+        setTableViewLayout()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func setTableViewLayout() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
 
 }
